9b3dcce0178fa517bd273ad86c0caf59c897b220
kukuh.utama <kukuh.utama@bankmandiri.co.id>
2019-07-03 20:13:37 +0700

    Add Spring Security

--- antasena-back/src/main/java/com/mandiri/antasena/service/impl/UserRoleServiceImpl.java
+++ antasena-back/src/main/java/com/mandiri/antasena/service/impl/UserRoleServiceImpl.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.mandiri.antasena.domain.UserRole;
+import com.mandiri.antasena.entity.RoleEntity;
+import com.mandiri.antasena.entity.UserEntity;
 import com.mandiri.antasena.entity.UserRoleEntity;
 import com.mandiri.antasena.repository.UserRoleRepository;
 import com.mandiri.antasena.service.UserRoleService;
@@ -17,11 +19,21 @@ public class UserRoleServiceImpl implements UserRoleService{
 	private UserRoleRepository userRoleRepository;
 	
 	private UserRoleEntity userRoleEntity;
+	private UserEntity userEntity;
+	private RoleEntity roleEntity;
 	
 	@Override
 	public UserRole save(UserRole userRole) {
 		userRoleEntity = new UserRoleEntity();
+		userEntity = new UserEntity();
+		roleEntity = new RoleEntity();
+		userEntity.fromDomain(userRole.getUser());
+		roleEntity.fromDomain(userRole.getRole());
+		
 		userRoleEntity.fromDomain(userRole, true);
+		userRoleEntity.setRoleEntity(roleEntity);
+		userRoleEntity.setUserEntity(userEntity);
+		
 		userRoleRepository.save(userRoleEntity);
 		return null;
 	}
